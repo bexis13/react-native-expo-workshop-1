03def41f9362da9dede6af79230ad2e1a7274052
maticaputti <matias.caputti@gmail.com>
2017-02-20 13:21:50 -0300

    Remove navigation

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
 import Exponent from 'exponent';
 import React from 'react';
-import MainStackNavigator from './navigators/MainStackNavigator';
+import { View, ScrollView, Platform,
+         StatusBar, StyleSheet } from 'react-native';
+import Header from './components/Header';
+import PhotoDetail from './components/PhotoDetail';
+import photosData from './data/photos.json';
 
 class AppContainer extends React.Component {
   render() {
     return (
-      <MainStackNavigator />
+      <View style={styles.container}>
+        <Header headerText={'Workshop - part 1'} />
+
+        <ScrollView>
+          <PhotoDetail photo={photosData[0]} />
+
+          {Platform.OS === 'ios' && <StatusBar barStyle="light-content" />}
+          {Platform.OS === 'android' && <View style={styles.statusBarUnderlay} />}
+        </ScrollView>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EEE',
+  },
+});
 
 Exponent.registerRootComponent(AppContainer);
